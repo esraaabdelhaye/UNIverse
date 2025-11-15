c85fccc56d3b612046fdca76dc12c9dded8706fe
Esraa Abdelhay Mohamed Mohamed <esraaabdelhay1984@gmail.com>
2025-11-15 20:06:56 +0200

    files added

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,18 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginPage } from './features/auth/pages/login-page/login-page';
-import { DoctorMainPage } from './features/doctor-dashboard/pages/doctor-main-page/doctor-main-page';
-import { StudentMain } from './features/student-dashboard/pages/student-main/student-main';
+
 export const routes: Routes = [
   {
     path: '',
     component: LoginPage,
-  },
-  {
-    path: 'doctor-dashboard',
-    component: DoctorMainPage,
-  },
-  {
-    path: 'student-dashboard',
-    component: StudentMain,
   },
 ];
