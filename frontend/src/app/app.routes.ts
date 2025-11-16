ed15132f99d9e5d3fde138f92542c22ef159ca7c
Esraa Abdelhay Mohamed Mohamed <esraaabdelhay1984@gmail.com>
2025-11-16 18:45:45 +0200

    login added

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,6 +4,11 @@ export const routes: Routes = [
   {
     path: '',
     loadChildren: () =>
+      import('./features/auth/pages/login/login-module').then((m) => m.LoginModule),
+  },
+  {
+    path: 'doctor-dashboard',
+    loadChildren: () =>
       import('./features/doctor-dashboard/doctor-dashboard-module').then(
         (m) => m.DoctorDashboardModule
       ),
