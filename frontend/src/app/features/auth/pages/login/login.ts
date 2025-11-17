f146aaac919e0f1c80f4b996e7ef75502f800b6e
Esraa Abdelhay Mohamed Mohamed <esraaabdelhay1984@gmail.com>
2025-11-17 14:23:42 +0200

    sign in function added

--- frontend/src/app/features/auth/pages/login/login.ts
+++ frontend/src/app/features/auth/pages/login/login.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-
+import { Router, Routes } from '@angular/router';
+import { ChooseRoleCard } from './components/choose-role-card/choose-role-card';
+import { ChooseAccount } from '../choose-account/choose-account';
+import { DoctorDashboard } from '../../../doctor-dashboard/doctor-dashboard';
 @Component({
   selector: 'app-login',
   imports: [],
@@ -7,5 +10,24 @@ import { Component } from '@angular/core';
   styleUrl: './login.css',
 })
 export class Login {
+  selectedRole = 'supervisor-prof'; // holds the chosen value
 
+  constructor(private router: Router) {}
+  selectRole(role: string) {
+    this.selectedRole = role;
+  }
+  LoginBtnClicked() {
+    console.log(this.selectedRole);
+    switch (this.selectedRole) {
+      case 'supervisor-prof':
+        this.router.navigate(['/doctor-dashboard']);
+        break;
+      case 'ta':
+        this.router.navigate(['/doctor-dashboard']);
+        break;
+      case 'student':
+        this.router.navigate(['/student-dashboard']);
+        break;
+    }
+  }
 }
