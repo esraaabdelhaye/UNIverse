941965f43b831a590840aa85394c189fd93b09d9
Youssef Ali Rashidi Ali <es.yousef.rashidy2023@alexu.edu.eg>
2025-11-30 19:02:40 +0200

    Added Security dependency , Data dependecny and MySQL driver and created hashing bean

--- backend/src/main/java/com/example/backend/repository/AnnouncementRepo.java
+++ backend/src/main/java/com/example/backend/repository/AnnouncementRepo.java
@@ -1,4 +1,4 @@
 package com.example.backend.repository;
 
-public class AnnouncementRepo {
+public interface AnnouncementRepo {
 }
--- backend/src/main/java/com/example/backend/repository/StudentGroupRepo.java
+++ backend/src/main/java/com/example/backend/repository/StudentGroupRepo.java
@@ -1,4 +1,4 @@
 package com.example.backend.repository;
 
-public class StudentGroupRepo {
+public interface StudentGroupRepo {
 }
