5806dc3506883ffcd0a10793f1d8e52ed6730ddb
Jana Rashed <es.jana.rashad2023@alexu.edu.eg>
2025-11-30 10:48:58 +0200

    TA pages edits

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,13 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DashBoard } from "./features/rep-dash-board/Views/dash-board/dash-board";
-import { RepDashBoard } from './features/rep-dash-board/rep-dash-board';
-import { Sidebar } from './features/rep-dash-board/Components/sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DashBoard, RepDashBoard, Sidebar],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
 })
 export class App {
